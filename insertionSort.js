05adee53e66e6e9038dbd1a33f9d44dfdfd90805
Aditya Potluri <adityapotluri1@gmail.com>
2020-07-24 17:21:07 -0400

    js finished

--- insertionSort.js
+++ insertionSort.js
@@ -1,1 +1,19 @@
-console.log("Insertion Sort")
+const numbers = [3,6,1,3,97,3,1,0,43];
+
+
+function insertionSort(numbers)  {
+    for (let i = 1; i < numbers.length; i++) {
+      let j = i - 1;
+      let currentNum = numbers[i];
+      while (j >= 0 && numbers[j] > currentNum) {
+        numbers[j + 1] = numbers[j];
+        j--;
+      }
+      numbers[j+1] = currentNum
+    }
+    return numbers
+  }
+
+console.log(insertionSort(numbers));
+
+
